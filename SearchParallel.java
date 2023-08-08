b00017519fc21c012f7a77f68abbe04efc3a18d4
KoffiesHyper <70659236+KoffiesHyper@users.noreply.github.com>
2023-08-08 20:25:03 +0200

    Update SearchParallel.java

--- SearchParallel.java
+++ SearchParallel.java
@@ -3,69 +3,60 @@
  * then moves downhill, stopping at the local minimum.
  */
 
-import java.util.Random;
 import java.util.concurrent.RecursiveTask;
 
-public class SearchParallel extends RecursiveTask<Integer> {	
-	private int id; // Searcher identifier
-	private int steps; // number of steps to end of search
-	private boolean stopped; // Did the search hit a previous trail?
-	private int numSearches;
+public class SearchParallel extends RecursiveTask<Integer> {
+	private int id;
+	private int steps;
+	private boolean stopped;
+
 	private int pos_row, pos_col;
-	
-	private static int nextID = 1;
-
-	private boolean DEBUG = false;
-
 	private TerrainArea terrain;
+	private int startIndex, endIndex;
 
-	protected static int SEQUENTIAL_CUTOFF = 100;
+	private static boolean DEBUG = false;
 
-	public SearchParallel(TerrainArea terrain, int numSearches, boolean DEBUG) {
-		this.terrain = terrain;
-		this.stopped = false;
-		this.numSearches = numSearches;
-		this.DEBUG = DEBUG;
+	protected static int SEQUENTIAL_CUTOFF = 10000;
+
+	private SearchParallel[] searches;
+
+	int min = Integer.MAX_VALUE;
+	int local_min = Integer.MAX_VALUE;
+	int finder = -1;
+
+	public SearchParallel(SearchParallel[] searches, int startIndex, int endIndex) {
+		this.searches = searches;
+		this.startIndex = startIndex;
+		this.endIndex = endIndex;
 	}
 
-	public SearchParallel(TerrainArea terrain, int row, int col, int id) {
+	public SearchParallel(int id, int row, int col, TerrainArea terrain) {
 		this.id = id;
-		this.terrain = terrain;
-		this.stopped = false;
 		this.pos_row = row;
 		this.pos_col = col;
+		this.stopped = false;
+		this.terrain = terrain;
 	}
 
 	protected Integer compute() {
+		if (endIndex - startIndex <= SEQUENTIAL_CUTOFF) {
+			for (int i = startIndex; i < endIndex; i++) {
+				local_min = searches[i].find_valleys();
+				// if ((!searches[i].isStopped()) && (local_min < min)) {
+				// 	min = local_min;
+				// 	finder = i;
+				// }
 
-		if (numSearches <= SEQUENTIAL_CUTOFF) {
-			int min = Integer.MAX_VALUE;
-			int local_min = Integer.MAX_VALUE;
-			int finder = -1;
-
-			for (int i = 0; i < numSearches; i++) {
-				Random rand = new Random();
-				int random_row = rand.nextInt(terrain.rows);
-				int random_col = rand.nextInt(terrain.columns);
-
-				SearchParallel search = new SearchParallel(terrain, random_row, random_col, nextID);
-				nextID++;
-
-				local_min = search.find_valleys();
-				if ((!search.isStopped()) && (local_min < min)) { // don't look at those who stopped because hit									// exisiting path
-					min = local_min;
-					finder = i; // keep track of who found it
-					
-					// System.out.println(nextID);
-					// if(DEBUG) System.out.println("Search "+search.getID()+" finished at  "+local_min + " in " +search.getSteps());
-				}
+				// if (DEBUG)
+				// 		System.out.println("Search " + searches[i].getID() + " finished at  " + local_min + " in "
+				// 				+ searches[i].getSteps());
 			}
 
-			return min;
-		}
-		else{
-			SearchParallel left = new SearchParallel(terrain, numSearches/2, DEBUG);
-			SearchParallel right = new SearchParallel(terrain, numSearches/2, DEBUG);
+			return 1;
+		} else {
+
+			SearchParallel left = new SearchParallel(searches, startIndex, (startIndex + endIndex) / 2);
+			SearchParallel right = new SearchParallel(searches, (startIndex + endIndex) / 2, endIndex);
 
 			left.fork();
 			int rightMin = right.compute();
@@ -75,32 +66,33 @@ public class SearchParallel extends RecursiveTask<Integer> {
 	}
 
 	public int find_valleys() {
-		int height = Integer.MAX_VALUE;
-		TerrainArea.Direction next = TerrainArea.Direction.STAY_HERE;
-		while (terrain.visited(pos_row, pos_col) == 0) { // stop when hit existing path
-			height = terrain.get_height(pos_row, pos_col);
-			terrain.mark_visited(pos_row, pos_col, id); // mark current position as visited
-			steps++;
-			next = terrain.next_step(pos_row, pos_col);
-			switch (next) {
-				case STAY_HERE:
-					return height; // found local valley
-				case LEFT:
-					pos_row--;
-					break;
-				case RIGHT:
-					pos_row = pos_row + 1;
-					break;
-				case UP:
-					pos_col = pos_col - 1;
-					break;
-				case DOWN:
-					pos_col = pos_col + 1;
-					break;
-			}
-		}
-		stopped = true;
-		return height;
+		return 1;
+		// int height = Integer.MAX_VALUE;
+		// TerrainArea.Direction next = TerrainArea.Direction.STAY_HERE;
+		// while (terrain.visited(pos_row, pos_col) == 0) { // stop when hit existing path
+		// 	height = terrain.get_height(pos_row, pos_col);
+		// 	terrain.mark_visited(pos_row, pos_col, id); // mark current position as visited
+		// 	steps++;
+		// 	next = terrain.next_step(pos_row, pos_col);
+		// 	switch (next) {
+		// 		case STAY_HERE:
+		// 			return height; // found local valley
+		// 		case LEFT:
+		// 			pos_row--;
+		// 			break;
+		// 		case RIGHT:
+		// 			pos_row = pos_row + 1;
+		// 			break;
+		// 		case UP:
+		// 			pos_col = pos_col - 1;
+		// 			break;
+		// 		case DOWN:
+		// 			pos_col = pos_col + 1;
+		// 			break;
+		// 	}
+		// }
+		// stopped = true;
+		// return height;
 	}
 
 	public int getID() {
